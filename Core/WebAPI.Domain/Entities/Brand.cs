437da2ce2d59aa89a7522b4ad38d30bbbd250a18
atkaya <atkaya>
2024-03-27 12:56:49 +0300

    created entities

---
+++ Core/WebAPI.Domain/Entities/Brand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Domain.Common;
+
+namespace WebAPI.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
---
+++ Core/WebAPI.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Domain.Entities
+{
+    public class Product
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int BrandId { get; set; }
+        public required decimal Price { get; set; }
+        public required decimal Discount { get; set; }
+        public Brand Brand { get; set; }
+        ICollection<Category> Categories { get; set; }
+    }
+}
