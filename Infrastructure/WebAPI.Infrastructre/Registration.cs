f4327831f33519f42e58dc6e2a7ebe8bad7747c5
atkaya <atkaya>
2024-04-03 10:15:00 +0300

    Token Service and CreateToken method are done

---
+++ Infrastructure/WebAPI.Infrastructre/Registration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Infrastructre.Tokens;
+
+namespace WebAPI.Infrastructre
+{
+    public static class Registration
+    {
+        public static void AddInfrastruce(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+        }
+    }
+}
