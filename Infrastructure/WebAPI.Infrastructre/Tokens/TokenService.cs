1f5ce7e28e4f30ae81f3383d49ee1156fca97ebe
atkaya <atkaya>
2024-04-03 14:30:35 +0300

    Token Service and Auth interface are done

--- Infrastructure/WebAPI.Infrastructre/Tokens/TokenService.cs
+++ Infrastructure/WebAPI.Infrastructre/Tokens/TokenService.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using WebAPI.Application.Interfaces.Tokens;
@@ -55,12 +56,31 @@ namespace WebAPI.Infrastructre.Tokens
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var randomNumber = new Byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
 
-        public ClaimsPrincipal? GetPrincipalExpiredToken()
+        public ClaimsPrincipal? GetPrincipalExpiredToken(string? token)
         {
-            throw new NotImplementedException();
+            TokenValidationParameters tokenValidationParameters = new() {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = false,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret)),
+                ValidateLifetime = false
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if(securityToken is not JwtSecurityToken jwtSecurityToken 
+                || !jwtSecurityToken.Header.Alg
+                .Equals(SecurityAlgorithms.HmacSha256,
+                StringComparison.InvariantCultureIgnoreCase)) 
+                throw new SecurityTokenException("Token bulunamadı.");
+            
+            return principal;
         }
     }
 }
--- Presentation/WebAPI.Api/Program.cs
+++ Presentation/WebAPI.Api/Program.cs
@@ -3,6 +3,8 @@ using WebAPI.Persistence;
 using WebAPI.Application;
 using WebAPI.Mapper;
 using WebAPI.Application.Exceptions;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +26,36 @@ builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddInfrastruce(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCustomMapper();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Web API", Version = "v1", Description = "Web API Swagger client." });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "'Bearer' yazýp boþluk býraktýktan sonra Token'ý girebilirsiniz \r\n\r\n Örnek: \"Baerer i_gr#8_obr3!6t3^*t^dt$i%j#9ml57!@q2nhi9a6o)(&_sdh^"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+
 
 var app = builder.Build();
 
