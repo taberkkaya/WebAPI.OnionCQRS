556047c04513b7b4b4755f952258656a1b8b303e
taberkkaya <taberkkaya@gmail.com>
2024-04-10 02:45:51 +0300

    Using cache is done

---
+++ Core/WebAPI.Application/Features/Brands/Commands/CreateBrand/CreateBrandCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Application.Bases;
+using WebAPI.Application.Interfaces.AutoMapper;
+using WebAPI.Application.UnitOfWorks;
+using WebAPI.Domain.Entities;
+
+namespace WebAPI.Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommandHandler : BaseHandler, IRequestHandler<CreateBrandCommandRequest, Unit>
+    {
+        public CreateBrandCommandHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+        }
+
+        public async Task<Unit> Handle(CreateBrandCommandRequest request, CancellationToken cancellationToken)
+        {
+            Faker faker = new();
+
+            List<Brand> brands = new();
+            
+            for(int i = 0; i < 1000000; i++)
+            {
+                brands.Add(new(faker.Commerce.Department(1)));
+            }
+
+            await unitOfWork.GetWriteRepository<Brand>().AddRangeAsync(brands);
+            await unitOfWork.SaveAsync();
+
+            return Unit.Value;
+        }
+    }
+}
